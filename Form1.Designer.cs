f27a73a42043ee949e351a283e38dece368b23e5
tiagorockman <tiagorockman@gmail.com>
2020-11-06 21:36:47 -0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace checkNetFramework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkText = new System.Windows.Forms.Label();
+            this.close = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextResult = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(178, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(484, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ".NET FRAMEWORK CHECK";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // checkText
+            // 
+            this.checkText.AutoSize = true;
+            this.checkText.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.checkText.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkText.Location = new System.Drawing.Point(46, 94);
+            this.checkText.Name = "checkText";
+            this.checkText.Size = new System.Drawing.Size(162, 21);
+            this.checkText.TabIndex = 2;
+            this.checkText.Text = "Verificando registro";
+            // 
+            // close
+            // 
+            this.close.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.close.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close.ForeColor = System.Drawing.Color.AliceBlue;
+            this.close.Location = new System.Drawing.Point(767, 0);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(30, 23);
+            this.close.TabIndex = 3;
+            this.close.Text = "X";
+            this.close.UseVisualStyleBackColor = false;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // richTextResult
+            // 
+            this.richTextResult.BackColor = System.Drawing.SystemColors.ControlText;
+            this.richTextResult.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextResult.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextResult.ForeColor = System.Drawing.SystemColors.Window;
+            this.richTextResult.Location = new System.Drawing.Point(41, 136);
+            this.richTextResult.MaximumSize = new System.Drawing.Size(676, 285);
+            this.richTextResult.Name = "richTextResult";
+            this.richTextResult.ReadOnly = true;
+            this.richTextResult.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextResult.Size = new System.Drawing.Size(676, 232);
+            this.richTextResult.TabIndex = 4;
+            this.richTextResult.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextResult);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.checkText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Framework Check";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label checkText;
+        private System.Windows.Forms.Button close;
+        private System.Windows.Forms.Timer timer1;
+        public System.Windows.Forms.RichTextBox richTextResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace checkNetFramework
+{
+    public partial class Form1 : Form
+    {
+        bool firstIn = true;
+        bool counter = false;
+        string text = "";
+        int len = 0;
+        int initialLen = 0;
+        string OutPutText = "";
+
+        //CODE FOR FORM MOVABLE
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            text = checkText.Text;
+            len = text.Length;
+            initialLen = len+3;
+            timer1.Start();          
+            
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.DoEvents();
+            timer1.Interval = 500; //espera 1 segundo
+            text = len < initialLen ? text + "." : text.Substring(0, len - 3);
+            len = text.Length;
+            checkText.Text = text;
+
+            if(counter)
+            {
+                checkText.Hide();
+                timer1.Stop();
+            }
+
+            if (firstIn)
+            {                
+                firstIn = false;
+                System.Windows.Forms.Application.DoEvents();
+                Task.Factory.StartNew(() =>
+                {
+                    Thread.Sleep(5000);
+                    //Executa pelo registro
+                    try
+                    {
+                        OutPutText = getFrameworks.MainExec();
+                    }
+                    catch (Exception)
+                    {
+                        //continue
+                    }
+                    
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        AppendText(OutPutText);
+                        text = "Verificando no cmd";
+                        len = text.Length;
+                        initialLen = len + 3;        
+
+                    });
+                    //realiza chamada da função sem interromper as ações na UI
+                    Thread.Sleep(1000);
+                    CmdInitiate();
+
+                });
+                
+            }
+
+        }
+
+        private  void CmdInitiate()
+        {
+            
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            //cmd.StartInfo.FileName = "powershell.exe";
+            //cmd.StartInfo.Arguments = @"/c dir \windows";
+            //cmd.StartInfo.Arguments = "wmic product get description | findstr /C:.NET";
+            string command = @"/c wmic product where ""Name like '%Net Framework%'"" get Name, Version";
+            cmd.StartInfo.Arguments = command;
+            //OutPutText += command + "\n";
+            
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.RedirectStandardError = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.RedirectStandardInput = false;
+
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.EnableRaisingEvents = true;
+
+            //cmd.OutputDataReceived += (a, b) => {
+            //    Debug.WriteLine(b.Data);               
+
+            //};
+
+            cmd.OutputDataReceived += (a, b) =>
+            {
+
+                OutPutText += $"{b.Data} \n";
+                Console.WriteLine(b.Data);
+            };
+            cmd.ErrorDataReceived += (a, b) =>
+            {
+
+                OutPutText += $"{b.Data} \n";
+                Console.WriteLine(b.Data);
+
+            };
+            //ao finalizar erro
+            cmd.Exited += (a, b) =>
+            {
+                counter = true;
+            };
+
+            //cmd.StartInfo.Arguments = "wmic product where \"Name like 'Microsoft .Net Framework%'\" get Name, Version";
+            cmd.Start();
+           // cmd.BeginErrorReadLine();
+            cmd.BeginOutputReadLine();
+            //string output = cmd.StandardOutput.ReadToEnd();
+            cmd.WaitForExit();
+
+
+            if (cmd.HasExited)
+            {
+                // MethodInvoker e BeginInvoke utilizado para que o método AppendText seja chamado utilizando a Thread Principal
+                this.Invoke((MethodInvoker)delegate
+                {
+                    AppendText(OutPutText);
+                });
+
+                //MethodInvoker mi = new MethodInvoker(this.AppendText);
+                //this.BeginInvoke(mi);
+                
+            }
+
+        }
+
+        public void AppendText(string val)
+        {
+            richTextResult.AppendText(val);
+        }
+    }
+}
